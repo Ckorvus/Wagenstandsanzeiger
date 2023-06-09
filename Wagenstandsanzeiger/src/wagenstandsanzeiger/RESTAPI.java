12b6dd91dd16368d5cfaa0cd4daca1dfa8817061
Tay Ho <Tay.Ho@hotmail.com>
2023-06-09 06:18:06 +0200

    eclipse auto format

--- Wagenstandsanzeiger/src/wagenstandsanzeiger/RESTAPI.java
+++ Wagenstandsanzeiger/src/wagenstandsanzeiger/RESTAPI.java
@@ -31,43 +31,45 @@ import java.util.stream.Collectors;
  */
 public class RESTAPI {
 	// Creates an array in which we will store the names of XML files
-    String[] pathnames;
-    static List<Sections> sectionList = new ArrayList<>();
-    
+	String[] pathnames;
+	static List<Sections> sectionList = new ArrayList<>();
+
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		RESTAPI restAPI = new RESTAPI();
 		restAPI.fetchSections();
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/station/", (exchange -> {
-        	String[] segments = exchange.getRequestURI().toString().split("/");
-        	if ("GET".equals(exchange.getRequestMethod())) {
-        		if (segments[1].equals("station") && segments[3].equals("train") && segments[5].equals("waggon")) {
-        			//String sections = restAPI.getStations(new String[]{segments[2], segments[4], segments[6]});
-        			Predicate<Sections> byStation = section -> section.getStation().equals(segments[2]);
-        			Predicate<Sections> byTrain = section -> section.getTrain().equals(segments[4]);
-        			Predicate<Sections> byWaggon = section -> section.getWaggon().equals(segments[6]);
-        			var responseText = sectionList.stream().filter(byStation).filter(byTrain).filter(byWaggon).collect(Collectors.toList()).get(0).getSections().toString();
-        			responseText = "{\"sections\":" + responseText + "}";
-        			exchange.sendResponseHeaders(200, responseText.getBytes().length);
-                    OutputStream output = exchange.getResponseBody();
-                    output.write(responseText.getBytes());
-                    output.flush();
-                } else {
-                    exchange.sendResponseHeaders(400, -1);// 400 Bad Request
-    	        }
-            } else {
-	            exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
-	        }
-            exchange.close();
-        }));
-        server.setExecutor(null);// creates a default executor
-        server.start();
+		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+		server.createContext("/station/", (exchange -> {
+			String[] segments = exchange.getRequestURI().toString().split("/");
+			if ("GET".equals(exchange.getRequestMethod())) {
+				if (segments[1].equals("station") && segments[3].equals("train") && segments[5].equals("waggon")) {
+					// String sections = restAPI.getStations(new String[]{segments[2], segments[4],
+					// segments[6]});
+					Predicate<Sections> byStation = section -> section.getStation().equals(segments[2]);
+					Predicate<Sections> byTrain = section -> section.getTrain().equals(segments[4]);
+					Predicate<Sections> byWaggon = section -> section.getWaggon().equals(segments[6]);
+					var responseText = sectionList.stream().filter(byStation).filter(byTrain).filter(byWaggon)
+							.collect(Collectors.toList()).get(0).getSections().toString();
+					responseText = "{\"sections\":" + responseText + "}";
+					exchange.sendResponseHeaders(200, responseText.getBytes().length);
+					OutputStream output = exchange.getResponseBody();
+					output.write(responseText.getBytes());
+					output.flush();
+				} else {
+					exchange.sendResponseHeaders(400, -1);// 400 Bad Request
+				}
+			} else {
+				exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+			}
+			exchange.close();
+		}));
+		server.setExecutor(null);// creates a default executor
+		server.start();
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -79,114 +81,128 @@ public class RESTAPI {
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		try {
-        // optional, but recommended
-        // process XML securely, avoid attacks like XML External Entities (XXE)
-        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        // parse XML file
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = null;
-        // Creates a new File instance by concatenating the current working directory with the first filename starting with the station abbrev.
-        File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
-        // Populates the array with names of XML files
-        pathnames = folder.list();
-        String FILENAME = "";
-        // For each pathname
-        for (String pathname : pathnames) {
-            // Print the names of files and directories
-        	if(pathname.startsWith(station + "_")) {
-            	FILENAME = pathname;
-            	doc = db.parse(new File(folder + "\\" + FILENAME));
-            	break;
-        	}
-        }
-        // optional, but recommended
-        doc.getDocumentElement().normalize();
-        // get every <track>
-        NodeList tracks = ((Element) doc.getElementsByTagName("tracks").item(0)).getElementsByTagName("track");
-        for (int i = 0; i < tracks.getLength(); i++) {
-        	 // get every <train>
-            NodeList trains = ((Element) ((Element) tracks.item(i)).getElementsByTagName("trains").item(0)).getElementsByTagName("train");
-            for (int j = 0; j < trains.getLength(); j++) {
-            	 // get every <trainNumber>
-                NodeList trainNumbers = ((Element) ((Element) trains.item(j)).getElementsByTagName("trainNumbers").item(0)).getElementsByTagName("trainNumber");
-            	 for (int k = 0; k < trainNumbers.getLength(); k++) {
-                 	if (trainNumbers.item(k).getTextContent().equals(trainNumber)) {
-                 		 // get every <waggon>
-                        NodeList waggons = ((Element) ((Element) trains.item(j)).getElementsByTagName("waggons").item(0)).getElementsByTagName("waggon");
-                         for (int l = 0; l < waggons.getLength(); l++) {
-                         	if (((Element) waggons.item(l)).getElementsByTagName("number").item(0).getTextContent().equals(waggonNumber)) {
-                         		 // get every <identifier>
-                                NodeList sections = ((Element) ((Element) waggons.item(l)).getElementsByTagName("sections").item(0)).getElementsByTagName("identifier");
-                         		for (int m = 0; m < sections.getLength(); m++) {
-                         			String section = sections.item(m).getTextContent();
-                         			response += (response.equals("")? "" : ",") + "\"" + section + "\"";
-                                 }
-                         		return response;
-                             }
-                         }
-                     }
-                 }
-            }
-        }
-    } catch (ParserConfigurationException | SAXException | IOException error) {
-    	error.printStackTrace();
-    }
-	return response;
+			// optional, but recommended
+			// process XML securely, avoid attacks like XML External Entities (XXE)
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			// parse XML file
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = null;
+			// Creates a new File instance by concatenating the current working directory
+			// with the first filename starting with the station abbrev.
+			File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
+			// Populates the array with names of XML files
+			pathnames = folder.list();
+			String FILENAME = "";
+			// For each pathname
+			for (String pathname : pathnames) {
+				// Print the names of files and directories
+				if (pathname.startsWith(station + "_")) {
+					FILENAME = pathname;
+					doc = db.parse(new File(folder + "\\" + FILENAME));
+					break;
+				}
+			}
+			// optional, but recommended
+			doc.getDocumentElement().normalize();
+			// get every <track>
+			NodeList tracks = ((Element) doc.getElementsByTagName("tracks").item(0)).getElementsByTagName("track");
+			for (int i = 0; i < tracks.getLength(); i++) {
+				// get every <train>
+				NodeList trains = ((Element) ((Element) tracks.item(i)).getElementsByTagName("trains").item(0))
+						.getElementsByTagName("train");
+				for (int j = 0; j < trains.getLength(); j++) {
+					// get every <trainNumber>
+					NodeList trainNumbers = ((Element) ((Element) trains.item(j)).getElementsByTagName("trainNumbers")
+							.item(0)).getElementsByTagName("trainNumber");
+					for (int k = 0; k < trainNumbers.getLength(); k++) {
+						if (trainNumbers.item(k).getTextContent().equals(trainNumber)) {
+							// get every <waggon>
+							NodeList waggons = ((Element) ((Element) trains.item(j)).getElementsByTagName("waggons")
+									.item(0)).getElementsByTagName("waggon");
+							for (int l = 0; l < waggons.getLength(); l++) {
+								if (((Element) waggons.item(l)).getElementsByTagName("number").item(0).getTextContent()
+										.equals(waggonNumber)) {
+									// get every <identifier>
+									NodeList sections = ((Element) ((Element) waggons.item(l))
+											.getElementsByTagName("sections").item(0))
+											.getElementsByTagName("identifier");
+									for (int m = 0; m < sections.getLength(); m++) {
+										String section = sections.item(m).getTextContent();
+										response += (response.equals("") ? "" : ",") + "\"" + section + "\"";
+									}
+									return response;
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException error) {
+			error.printStackTrace();
+		}
+		return response;
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public void fetchSections() {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		try {
-        // optional, but recommended
-        // process XML securely, avoid attacks like XML External Entities (XXE)
-        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        // parse XML file
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = null;
-        // Creates a new File instance by concatenating the current working directory with the first filename starting with the station abbrev.
-        File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
-        // Populates the array with names of XML files
-        pathnames = folder.list();
-        // For each pathname
-        for (String pathname : pathnames) {
-            // Print the names of files and directories
-    		String station = pathname.split("_", 2)[0];
-        	doc = db.parse(new File(folder + "\\" + pathname));
+			// optional, but recommended
+			// process XML securely, avoid attacks like XML External Entities (XXE)
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			// parse XML file
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = null;
+			// Creates a new File instance by concatenating the current working directory
+			// with the first filename starting with the station abbrev.
+			File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
+			// Populates the array with names of XML files
+			pathnames = folder.list();
+			// For each pathname
+			for (String pathname : pathnames) {
+				// Print the names of files and directories
+				String station = pathname.split("_", 2)[0];
+				doc = db.parse(new File(folder + "\\" + pathname));
 
-            // optional, but recommended
-            doc.getDocumentElement().normalize();
-            // get every <track>
-            NodeList tracks = ((Element) doc.getElementsByTagName("tracks").item(0)).getElementsByTagName("track");
-            for (int i = 0; i < tracks.getLength(); i++) {
-            	 // get every <train>
-                NodeList trains = ((Element) ((Element) tracks.item(i)).getElementsByTagName("trains").item(0)).getElementsByTagName("train");
-                for (int j = 0; j < trains.getLength(); j++) {
-                	 // get every <trainNumber>
-                    NodeList trainNumbers = ((Element) ((Element) trains.item(j)).getElementsByTagName("trainNumbers").item(0)).getElementsByTagName("trainNumber");
-                	 for (int k = 0; k < trainNumbers.getLength(); k++) {
-                		 String trainNumber = trainNumbers.item(k).getTextContent();
-                     		 // get every <waggon>
-                            NodeList waggons = ((Element) ((Element) trains.item(j)).getElementsByTagName("waggons").item(0)).getElementsByTagName("waggon");
-                             for (int l = 0; l < waggons.getLength(); l++) {
-                            	 String waggonNumber = ((Element) waggons.item(l)).getElementsByTagName("number").item(0).getTextContent();
-                             		 // get every <identifier>
-                                    NodeList sections = ((Element) ((Element) waggons.item(l)).getElementsByTagName("sections").item(0)).getElementsByTagName("identifier");
-                                    List<String> section = new ArrayList<>();
-                             		for (int m = 0; m < sections.getLength(); m++) {
-                             			section.add("\"" + sections.item(m).getTextContent() + "\"");
-                                     }
-                             		((List<Sections>) sectionList).add(new Sections(station, trainNumber, waggonNumber, section));
-                             }
-                     }
-                }
-            }
-        }
-    } catch (ParserConfigurationException | SAXException | IOException error) {
-    	error.printStackTrace();
-    }
-	return;
+				// optional, but recommended
+				doc.getDocumentElement().normalize();
+				// get every <track>
+				NodeList tracks = ((Element) doc.getElementsByTagName("tracks").item(0)).getElementsByTagName("track");
+				for (int i = 0; i < tracks.getLength(); i++) {
+					// get every <train>
+					NodeList trains = ((Element) ((Element) tracks.item(i)).getElementsByTagName("trains").item(0))
+							.getElementsByTagName("train");
+					for (int j = 0; j < trains.getLength(); j++) {
+						// get every <trainNumber>
+						NodeList trainNumbers = ((Element) ((Element) trains.item(j))
+								.getElementsByTagName("trainNumbers").item(0)).getElementsByTagName("trainNumber");
+						for (int k = 0; k < trainNumbers.getLength(); k++) {
+							String trainNumber = trainNumbers.item(k).getTextContent();
+							// get every <waggon>
+							NodeList waggons = ((Element) ((Element) trains.item(j)).getElementsByTagName("waggons")
+									.item(0)).getElementsByTagName("waggon");
+							for (int l = 0; l < waggons.getLength(); l++) {
+								String waggonNumber = ((Element) waggons.item(l)).getElementsByTagName("number").item(0)
+										.getTextContent();
+								// get every <identifier>
+								NodeList sections = ((Element) ((Element) waggons.item(l))
+										.getElementsByTagName("sections").item(0)).getElementsByTagName("identifier");
+								List<String> section = new ArrayList<>();
+								for (int m = 0; m < sections.getLength(); m++) {
+									section.add("\"" + sections.item(m).getTextContent() + "\"");
+								}
+								((List<Sections>) sectionList)
+										.add(new Sections(station, trainNumber, waggonNumber, section));
+							}
+						}
+					}
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException error) {
+			error.printStackTrace();
+		}
+		return;
 	}
 }
--- Wagenstandsanzeiger/src/wagenstandsanzeiger/Sections.java
+++ Wagenstandsanzeiger/src/wagenstandsanzeiger/Sections.java
@@ -4,47 +4,47 @@ import java.util.List;
 
 public class Sections {
 
-    private String station;
-    private String train;
-    private String waggon;
-    private List<String> sections;
+	private String station;
+	private String train;
+	private String waggon;
+	private List<String> sections;
 
-    public Sections(String station, String train, String waggon, List<String> sections) {
-        this.station = station;
-        this.train = train;
-        this.waggon = waggon;
-        this.sections = sections;
-    }
+	public Sections(String station, String train, String waggon, List<String> sections) {
+		this.station = station;
+		this.train = train;
+		this.waggon = waggon;
+		this.sections = sections;
+	}
 
-    public String getStation() {
-        return station;
-    }
+	public String getStation() {
+		return station;
+	}
 
-    public void setStation(String station) {
-        this.station = station;
-    }
+	public void setStation(String station) {
+		this.station = station;
+	}
 
-    public String getTrain() {
-        return train;
-    }
+	public String getTrain() {
+		return train;
+	}
 
-    public void setTrain(String train) {
-        this.train = train;
-    }
+	public void setTrain(String train) {
+		this.train = train;
+	}
 
-    public String getWaggon() {
-        return waggon;
-    }
+	public String getWaggon() {
+		return waggon;
+	}
 
-    public void setWaggon(String waggon) {
-        this.waggon = waggon;
-    }
+	public void setWaggon(String waggon) {
+		this.waggon = waggon;
+	}
 
-    public List<String> getSections() {
-        return sections;
-    }
+	public List<String> getSections() {
+		return sections;
+	}
 
-    public void setSections(List<String> sections) {
-        this.sections = sections;
-    }
+	public void setSections(List<String> sections) {
+		this.sections = sections;
+	}
 }
