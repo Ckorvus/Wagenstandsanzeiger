e3ca18c44d52e0ed804985354ac47d084c39c287
Tay Ho <Tay.Ho@hotmail.com>
2023-06-20 21:54:33 +0200

    added detailed comments

--- Wagenstandsanzeiger/src/wagenstandsanzeiger/RESTAPI.java
+++ Wagenstandsanzeiger/src/wagenstandsanzeiger/RESTAPI.java
@@ -30,8 +30,9 @@ import java.util.stream.Collectors;
  *
  */
 public class RESTAPI {
-	// Creates an array in which we will store the names of XML files
+	// an array in which XML file names will be stored
 	String[] pathnames;
+	// a list with all sections from the XML files
 	static List<Sections> sectionList = new ArrayList<>();
 
 	/**
@@ -40,20 +41,29 @@ public class RESTAPI {
 	 */
 	public static void main(String[] args) throws IOException {
 		RESTAPI restAPI = new RESTAPI();
-		restAPI.fetchSections();
+		restAPI.fetchSections(); // initialize List<Sections> sectionList once for improved method
 		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 		server.createContext("/station/", (exchange -> {
 			String[] segments = exchange.getRequestURI().toString().split("/");
 			if ("GET".equals(exchange.getRequestMethod())) {
+				//check if URL is well formed
 				if (segments[1].equals("station") && segments[3].equals("train") && segments[5].equals("waggon")) {
-					// String sections = restAPI.getStations(new String[]{segments[2], segments[4],
-					// segments[6]});
+					/* alternative simple method start
+					String responseText = restAPI.getStations(new String[]{segments[2], segments[4], segments[6]});
+					responseText = "{\"sections\":[" + responseText + "]}";
+					// alternative simple method end*/
+					
+					// improved method start
+					// definition of predicates which evaluate to boolean
 					Predicate<Sections> byStation = section -> section.getStation().equals(segments[2]);
 					Predicate<Sections> byTrain = section -> section.getTrain().equals(segments[4]);
 					Predicate<Sections> byWaggon = section -> section.getWaggon().equals(segments[6]);
-					var responseText = sectionList.stream().filter(byStation).filter(byTrain).filter(byWaggon)
+					//usage of the predicates in JavaScript style list filters
+					String responseText = sectionList.stream().filter(byStation).filter(byTrain).filter(byWaggon)
 							.collect(Collectors.toList()).get(0).getSections().toString();
 					responseText = "{\"sections\":" + responseText + "}";
+					// improved method end
+
 					exchange.sendResponseHeaders(200, responseText.getBytes().length);
 					OutputStream output = exchange.getResponseBody();
 					output.write(responseText.getBytes());
@@ -66,12 +76,14 @@ public class RESTAPI {
 			}
 			exchange.close();
 		}));
-		server.setExecutor(null);// creates a default executor
+		server.setExecutor(null); // creates a default executor
 		server.start();
 	}
 
 	/**
 	 * @param args
+	 * simple getter method
+	 * iterates with brute force through all XML files with every GET request
 	 */
 	public String getStations(String[] args) {
 		String station = args[0];
@@ -87,15 +99,15 @@ public class RESTAPI {
 			// parse XML file
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = null;
-			// Creates a new File instance by concatenating the current working directory
-			// with the first filename starting with the station abbrev.
+			// create a new File instance by concatenating the current working directory
+			// with the first filename starting with the station abbreviation
 			File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
 			// Populates the array with names of XML files
 			pathnames = folder.list();
 			String FILENAME = "";
 			// For each pathname
 			for (String pathname : pathnames) {
-				// Print the names of files and directories
+				// assemble the names of files and directories
 				if (pathname.startsWith(station + "_")) {
 					FILENAME = pathname;
 					doc = db.parse(new File(folder + "\\" + FILENAME));
@@ -126,6 +138,7 @@ public class RESTAPI {
 									NodeList sections = ((Element) ((Element) waggons.item(l))
 											.getElementsByTagName("sections").item(0))
 											.getElementsByTagName("identifier");
+									// add quotation marks to the output
 									for (int m = 0; m < sections.getLength(); m++) {
 										String section = sections.item(m).getTextContent();
 										response += (response.equals("") ? "" : ",") + "\"" + section + "\"";
@@ -145,6 +158,9 @@ public class RESTAPI {
 
 	/**
 	 * @param args
+	 * improved fetch-once method
+	 * iterates with through all XML files and saves every fetched section into memory
+	 * only executed once the HTTP server starts
 	 */
 	public void fetchSections() {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -155,19 +171,19 @@ public class RESTAPI {
 			// parse XML file
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = null;
-			// Creates a new File instance by concatenating the current working directory
-			// with the first filename starting with the station abbrev.
+			// create a new File instance by concatenating the current working directory
+			// with the first filename starting with the station abbreviation
 			File folder = new File(System.getProperty("user.dir") + "\\src\\wagenstandsanzeiger\\resource\\");
-			// Populates the array with names of XML files
+			// populates the array with names of XML files
 			pathnames = folder.list();
 			// For each pathname
 			for (String pathname : pathnames) {
-				// Print the names of files and directories
+				// assemble the names of files and directories
 				String station = pathname.split("_", 2)[0];
 				doc = db.parse(new File(folder + "\\" + pathname));
-
 				// optional, but recommended
 				doc.getDocumentElement().normalize();
+
 				// get every <track>
 				NodeList tracks = ((Element) doc.getElementsByTagName("tracks").item(0)).getElementsByTagName("track");
 				for (int i = 0; i < tracks.getLength(); i++) {
@@ -190,9 +206,11 @@ public class RESTAPI {
 								NodeList sections = ((Element) ((Element) waggons.item(l))
 										.getElementsByTagName("sections").item(0)).getElementsByTagName("identifier");
 								List<String> section = new ArrayList<>();
+								// add quotation marks to the output
 								for (int m = 0; m < sections.getLength(); m++) {
 									section.add("\"" + sections.item(m).getTextContent() + "\"");
 								}
+								// save fetched section into memory
 								((List<Sections>) sectionList)
 										.add(new Sections(station, trainNumber, waggonNumber, section));
 							}
